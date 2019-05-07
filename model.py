22ccd3b5d3942cd0f5b40d3e8f7145490a29467a
Nitin Kishore Sai Samala <snk.nitin@gmail.com>
2019-05-07 10:16:34 -0700

    Using default actor and critics from sample ddpg code without any batchnorm or dropout

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size,hidden_sizes, seed, fc1_units=128,fc2_units = 32):
+    def __init__(self, state_size, action_size,hidden_sizes, seed, fc1_units=400,fc2_units = 300):
         """Initialize parameters and build model.
         Params
         ======
@@ -23,32 +23,41 @@ class Actor(nn.Module):
             fc2_units (int): Number of nodes in second hidden layer
         """
         super(Actor, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        # self.fc1 = nn.Linear(state_size, fc1_units)
+        # self.bn1 = nn.BatchNorm1d(fc1_units)
+        # self.fc2 = nn.Linear(fc1_units, fc2_units)
+        # self.bn2 = nn.BatchNorm1d(fc2_units)
+        # self.out = nn.Linear(fc2_units, action_size)
+
         self.fc1 = nn.Linear(state_size, fc1_units)
-        self.bn1 = nn.BatchNorm1d(fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.bn2 = nn.BatchNorm1d(fc2_units)
-        self.out = nn.Linear(fc2_units, action_size)
+        self.fc3 = nn.Linear(fc2_units, action_size)
         self.reset_parameters()
+
 
     def reset_parameters(self):
         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.out.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        if state.dim() == 1:
-            state = torch.unsqueeze(state, 0)
-
-        x = self.bn1(F.leaky_relu(self.fc1(state)))
-        x = self.bn2(F.leaky_relu(self.fc2(x)))
-        return torch.tanh(self.out(x))
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        # if state.dim() == 1:
+        #     state = torch.unsqueeze(state, 0)
+        #
+        # x = self.bn1(F.leaky_relu(self.fc1(state)))
+        # x = self.bn2(F.leaky_relu(self.fc2(x)))
+        # return torch.tanh(self.out(x))
+        return torch.tanh(self.fc3(x))
 
 
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=128, fc2_units=128, fc3_units=128,keep_prob=0.2):
+    def __init__(self, state_size, action_size,num_agents, seed, fcs1_units=400, fc2_units=300,keep_prob=0.2):
         """Initialize parameters and build model.
         Params
         ======
@@ -61,26 +70,33 @@ class Critic(nn.Module):
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
 
-        self.fcs1 = nn.Linear(state_size, fcs1_units)
-        self.fc2 = nn.Linear(fcs1_units + action_size, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, fc3_units)
-        self.out = nn.Linear(fc3_units, 1)
+        # self.fcs1 = nn.Linear(state_size, fcs1_units)
+        # self.fc2 = nn.Linear(fcs1_units + action_size, fc2_units)
+        # self.fc3 = nn.Linear(fc2_units, fc3_units)
+        # self.out = nn.Linear(fc3_units, 1)
 
+        self.fcs1 = nn.Linear((state_size + action_size) * num_agents, fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
 
         self.reset_parameters()
 
     def reset_parameters(self):
         self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
-        self.out.weight.data.uniform_(-3e-3, 3e-3)
+        #self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
         # Random hack to avoid mismatch error
-        xs = F.leaky_relu(self.fcs1(state.view(-1, 48)))
-        x = torch.cat((xs, action.view(-1, 4)), dim=1)
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
-
-        return self.out(x)
+        xs = torch.cat((state, action.float()), dim=1)
+        x = F.relu(self.fcs1(xs))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+        # xs = F.leaky_relu(self.fcs1(state.view(-1, 48)))
+        # x = torch.cat((xs, action.view(-1, 4)), dim=1)
+        # x = F.leaky_relu(self.fc2(x))
+        # x = F.leaky_relu(self.fc3(x))
+        #
+        # return self.out(x)
