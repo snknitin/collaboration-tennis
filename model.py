7fbb12a5883d92407c7d49e61c4fe2edba586440
Nitin Kishore Sai Samala <snk.nitin@gmail.com>
2019-05-09 22:07:02 -0700

    Modified architecture with batchnorm and dropout

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=400,fc2_units = 300):
+    def __init__(self, state_size, action_size, seed, fc1_units=32,fc2_units = 16):
         """Initialize parameters and build model.
         Params
         ======
@@ -24,40 +24,39 @@ class Actor(nn.Module):
         """
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
-        # self.fc1 = nn.Linear(state_size, fc1_units)
-        # self.bn1 = nn.BatchNorm1d(fc1_units)
-        # self.fc2 = nn.Linear(fc1_units, fc2_units)
-        # self.bn2 = nn.BatchNorm1d(fc2_units)
-        # self.out = nn.Linear(fc2_units, action_size)
+        self.dropout = nn.Dropout(p=0.2)
 
         self.fc1 = nn.Linear(state_size, fc1_units)
+        self.bn1 = nn.BatchNorm1d(fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.bn2 = nn.BatchNorm1d(fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
         self.reset_parameters()
 
 
     def reset_parameters(self):
-        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc1.weight.data.xavier_uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.xavier_uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.xavier_uniform_(-3e-3, 3e-3)
+
+        self.fc1.bias.data.fill_(0.01)
+        self.fc2.bias.data.fill_(0.01)
+        self.fc3.bias.data.fill_(0.01)
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        x = F.leaky_relu(self.fc1(state))
-        x = F.leaky_relu(self.fc2(x))
-        # if state.dim() == 1:
-        #     state = torch.unsqueeze(state, 0)
-        #
-        # x = self.bn1(F.leaky_relu(self.fc1(state)))
-        # x = self.bn2(F.leaky_relu(self.fc2(x)))
-        # return torch.tanh(self.out(x))
-        return torch.tanh(self.fc3(x))
+        if state.dim() == 1:
+            state = torch.unsqueeze(state, 0)
+        x = self.bn1(F.leaky_relu(self.fc1(state)))
+        x = self.bn2(F.leaky_relu(self.fc2(self.dropout(x))))
+
+        return torch.tanh(self.fc3(self.dropout(x)))
 
 
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size,num_agents, seed, fcs1_units=400, fc2_units=300,keep_prob=0.2):
+    def __init__(self, state_size, action_size,num_agents, seed, fcs1_units=32, fc2_units=16,keep_prob=0.2):
         """Initialize parameters and build model.
         Params
         ======
@@ -74,29 +73,32 @@ class Critic(nn.Module):
         # self.fc2 = nn.Linear(fcs1_units + action_size, fc2_units)
         # self.fc3 = nn.Linear(fc2_units, fc3_units)
         # self.out = nn.Linear(fc3_units, 1)
-        #self.dropout = nn.Dropout(p=keep_prob)
+        self.dropout = nn.Dropout(p=keep_prob)
         self.fcs1 = nn.Linear((state_size + action_size) * num_agents, fcs1_units)
+        self.bn1 = nn.BatchNorm1d(fcs1_units)
         self.fc2 = nn.Linear(fcs1_units, fc2_units)
+        self.bn2 = nn.BatchNorm1d(fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fcs1.weight.data.xavier_uniform_(*hidden_init(self.fcs1))
+        self.fc2.weight.data.xavier_uniform_(*hidden_init(self.fc2))
         #self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
-        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc3.weight.data.xavier_uniform_(-3e-3, 3e-3)
+
+        self.fcs1.bias.data.fill_(0.01)
+        self.fc2.bias.data.fill_(0.01)
+        self.fc3.bias.data.fill_(0.01)
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
         # Random hack to avoid mismatch error
+        if state.dim() == 1:
+            state = torch.unsqueeze(state, 0)
         xs = torch.cat((state, action.float()), dim=1)
-        x = F.leaky_relu(self.fcs1(xs))
-        x = F.leaky_relu(self.fc2(x))
-        return self.fc3(x)
-        # xs = F.leaky_relu(self.fcs1(state.view(-1, 48)))
-        # x = torch.cat((xs, action.view(-1, 4)), dim=1)
-        # x = F.leaky_relu(self.fc2(x))
-        # x = F.leaky_relu(self.fc3(x))
-        #
-        # return self.out(x)
+        x = self.bn1(F.leaky_relu(self.fcs1(xs)))
+        x = self.bn2(F.leaky_relu(self.fc2(self.dropout(x))))
+        return self.fc3(self.dropout(x))
+
