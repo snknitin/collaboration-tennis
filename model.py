8804d5aed50779f1db2aff4ec57bffd4ac205e25
Nitin Kishore Sai Samala <snk.nitin@gmail.com>
2019-05-13 10:59:18 -0700

    Modified architecture and hidden layer sizes as per the feedback for the project

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=256,fc2_units = 128):
+    def __init__(self, state_size, action_size, seed, fc1_units=128,fc2_units = 128):
         """Initialize parameters and build model.
         Params
         ======
@@ -31,8 +31,8 @@ class Actor(nn.Module):
         # self.out = nn.Linear(fc2_units, action_size)
 
         self.dropout = nn.Dropout(p=0.2)
-
-        self.fc1 = nn.Linear(state_size*2 , fc1_units)
+        self.bn0 = nn.BatchNorm1d(state_size)
+        self.fc1 = nn.Linear(state_size , fc1_units)
         self.bn1 = nn.BatchNorm1d(fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.bn2 = nn.BatchNorm1d(fc2_units)
@@ -56,7 +56,7 @@ class Actor(nn.Module):
             # print(state.shape)
             state = torch.unsqueeze(state, 0)
         # print(state.shape)
-        x = self.fc1(state)
+        x = self.fc1(self.bn0(state))
         # print(x.shape)
         x = self.bn1(F.relu(x))
         x = self.bn2(F.relu(self.fc2(x)))
@@ -67,7 +67,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=256, fc2_units=128,keep_prob=0.2):
+    def __init__(self, state_size, action_size, seed, fcs1_units=128, fc2_units=128,keep_prob=0.2):
         """Initialize parameters and build model.
         Params
         ======
@@ -86,10 +86,10 @@ class Critic(nn.Module):
         # self.out = nn.Linear(fc3_units, 1)
         #self.dropout = nn.Dropout(p=keep_prob)
         self.dropout = nn.Dropout(p=keep_prob)
-        self.fcs1 = nn.Linear((state_size)*2 +action_size, fcs1_units)
+        self.fcs1 = nn.Linear(state_size, fcs1_units)
         self.bn1 = nn.BatchNorm1d(fcs1_units)
-        self.fc2 = nn.Linear(fcs1_units, fc2_units)
-        self.bn2 = nn.BatchNorm1d(fc2_units)
+        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
+        #self.bn2 = nn.BatchNorm1d(fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
 
         self.reset_parameters()
@@ -111,8 +111,9 @@ class Critic(nn.Module):
         # Random hack to avoid mismatch error
         if state.dim() == 1:
             state = torch.unsqueeze(state, 0)
-        xs = torch.cat((state, action.float()), dim=1)
-        x = self.bn1(F.relu(self.fcs1(xs)))
-        x = self.bn2(F.relu(self.fc2(x)))
+        xs = self.bn1(F.relu(self.fcs1(state)))
+        xs = torch.cat((xs, action.float()), dim=1)
+
+        x = self.bn2(F.relu(self.fc2(xs)))
         return self.fc3(x)
 
